a41982b91f08af6349dc0f1aaa3c636e1674e3b3
shitwolfymakes <bhawk11@gmail.com>
2020-03-29 01:44:53 -0500

    removed redundant code

--- demo/demo_movies.py
+++ demo/demo_movies.py
@@ -21,56 +21,6 @@ def main():
     #end if
     interk = interkonnect.Interkonnect(data)
 
-    """
-    commands_queue = Queue()
-    imap_obj = imapclient.IMAPClient('imap.gmail.com', ssl=True)
-    imap_obj.login(data.get("email"), data.get("pass"))
-    imap_obj.select_folder('INBOX')
-    email_ids = imap_obj.search('UNSEEN')
-    if email_ids:
-        for email_id in email_ids:
-            # get data from the email
-            tokens = []
-            print("email id: %s" % email_id)
-            raw_msg = imap_obj.fetch([email_id], ['BODY[]', 'FLAGS'])
-            message = pyzmail.PyzMessage.factory(raw_msg[email_id][b'BODY[]'])
-            print(message.get_address('from'))
-            if message.text_part is not None:
-                payload = message.text_part.get_payload().decode('utf-8')
-                tokens = payload.split()[0:6]
-                #print("plain text:", tokens)
-            elif message.html_part is not None:
-                payload = message.html_part.get_payload().decode('utf-8')
-                soup = BeautifulSoup(payload, "html.parser")
-                tokens = soup.text.rstrip().split()[0:6]
-                #print("html:", tokens)
-            elif message.mailparts[0].type.startswith('text/'):
-                mailpart = message.mailparts[0]
-                payload, used_charset = pyzmail.decode_text(mailpart.get_payload(), mailpart.charset, None)
-                tokens = payload.split()[0:6]
-                #print("plain text:", tokens)
-            else:
-                #TODO: return error
-                pass
-            #end if/elif/else
-
-            # handling carrier extra shit
-            if 'T-Mobile' in tokens:
-                # tmomail.net
-                tokens.remove('T-Mobile')
-            elif 'Multimedia' in tokens[0] and 'Message' in tokens[1]:
-                # mms.att.net
-                tokens.remove(tokens[0])
-                tokens.remove(tokens[0])
-            #end if/elif
-
-            print(tokens)
-            print()
-            commands_queue.put(tokens)
-        #end for
-    #end if
-    print(list(commands_queue.queue))
-    """
     # main listener loop
     while True:
         print("in listener")
