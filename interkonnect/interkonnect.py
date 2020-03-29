f0b6aa8b340b618c288918d7a4548828ba9ac065
shitwolfymakes <bhawk11@gmail.com>
2020-03-29 01:12:12 -0500

    init now sets up the client session

--- interkonnect/interkonnect.py
+++ interkonnect/interkonnect.py
@@ -3,14 +3,20 @@
 
 from queue import Queue
 
+import imapclient
+
 
 class Interkonnect:
     def __init__(self, ik_data):
         self.ik_data = ik_data
-        self.creds = (ik_data.get("email"), ik_data.get("pass"))
         self.wait_time = ik_data.get("wait_time")
 
         self.commands_queue = None
+
+        # login to the
+        self.imap_obj = imapclient.IMAPClient('imap.gmail.com', ssl=True)
+        self.imap_obj.login(ik_data.get("email"), ik_data.get("pass"))
+        self.imap_obj.select_folder('INBOX')
     #end init
 
     def check_inbox(self):
