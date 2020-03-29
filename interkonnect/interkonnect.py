251b96fe36e5f4d317259c76111aa241a1379d4e
shitwolfymakes <bhawk11@gmail.com>
2020-03-29 01:08:22 -0500

    declared variables and method

--- interkonnect/interkonnect.py
+++ interkonnect/interkonnect.py
@@ -1,8 +1,19 @@
 """ Interconnect allows control of programs/devices via SMS """
 
 
+from queue import Queue
+
+
 class Interkonnect:
-    def __init__(self):
-        pass
+    def __init__(self, ik_data):
+        self.ik_data = ik_data
+        self.creds = (ik_data.get("email"), ik_data.get("pass"))
+        self.wait_time = ik_data.get("wait_time")
+
+        self.commands_queue = None
     #end init
+
+    def check_inbox(self):
+        self.commands_queue = Queue()
+    #end check_inbox
 #end Interkonnect
